bfca351ec644c69f4ed4d43e05ef41a651ee1cc9
skrall3119 <alexjanci@yahoo.com>
2020-09-20 13:28:46 -0500

    Initial Commit

---
+++ MySecondApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Alex Janci on 9/20/20.
+//  Copyright © 2020 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
