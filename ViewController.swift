1865aba51d03320d3269eab93c6924f18c2d8d1c
Alex Janci <ajanci429@students.mchenry.edu>
2021-10-26 23:12:01 -0500

    Add files via upload
    init

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySecondApp
+//
+//  Created by Alex Janci on 10/26/21.
+//  Copyright © 2021 Alex Janci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
